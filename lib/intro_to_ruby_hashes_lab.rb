209d0137ebe757fe9497ea2c343383bd68cec2d1
Doug K <doug.kerzner@gmail.com>
2021-01-18 19:22:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  while hash[key] 
+ hash[key] #!= nil
+    
 end
 
 def update_counting_hash(hash, key)
